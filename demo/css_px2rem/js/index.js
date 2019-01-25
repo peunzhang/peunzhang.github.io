37649bece5b145236374f3e661d7fc362aad8e63
peunzhang <peunzhang@tencent.com>
2019-01-25 10:40:17 +0800

    px2rem补充浮点数的运算

--- demo/css_px2rem/js/index.js
+++ demo/css_px2rem/js/index.js
@@ -1,5 +1,6 @@
 import {tips} from './lib/fn.js'
 import {cssbeautify} from '../../js/cssbeautify.js'
+import NP from '../../js/number-precision.js'
 
 
 var app = new Vue({
@@ -25,37 +26,38 @@ var app = new Vue({
         converValue : function(){
             let that = this;
             let textValue = that.textValue;
-            let _string;
-            let unitValue = that.unitValue;
-            if(textValue.length > 0){
-                _string = cssbeautify(textValue,{
+            let unitValue = parseFloat(that.unitValue);
+            let matchPositive = /\d+(\.\d+)?/;//正浮点数 + 0
+            if(!matchPositive.test(unitValue) || unitValue == 0){
+                tips(that,'请输入正确的换算值')
+            }
+            else if(textValue.length > 0){
+                let arr = cssbeautify(textValue,{
                     indent: '  ',
                     openbrace: 'end-of-line',
                     autosemicolon: true
-                });
+                }).split("\n")
 
-                let arr = _string.split("\n");
-                let _string_new = '';
+                let _textValue = '';
                 for (let i = 0; i < arr.length; i++) {
-                    let line = arr[i];
-                    _string_new += line.replace(/\d+(\.\d+)?\s*px/g, function (px) {
+                    let arrChild = arr[i];
+                    _textValue += arrChild.replace(/\d+(\.\d+)?\s*px/, function (value) {
 						if (that.unHandleCheckbox) {
-                            if (!/border:/ig.test(line)) {
-                                return (parseFloat(px) / parseFloat(unitValue)) + "rem";
+                            if (!/border:/ig.test(arrChild)) {
+                                return NP.round(parseFloat(value) / unitValue, 4)+ "rem";
                             } else {
-                                return px;
+                                return value;
                             }
 						}
 						else{
-                            return (parseFloat(px) / parseFloat(unitValue)) + "rem";
+                            return NP.round(parseFloat(value) / unitValue, 4)+ "rem";
 						}
-
 
 					}) + "\n";
 
 
                 }
-                that.resultValue = _string_new;
+                that.resultValue = _textValue;
             }
             else{
                 tips(that,'请粘贴代码')
